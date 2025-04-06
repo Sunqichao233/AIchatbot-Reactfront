414a217d2e955fb2a08f41291828d7fddedfee87
孙琪超 <sunqichao@sunqichaodeMacBook-Pro.local>
2025-04-06 22:13:09 +0900

    新增注册和找回密码页面并且可以跳转

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom"
 import LoginForm from "@/components/LoginForm"
+import RegisterForm from "@/components/RegisterForm"
+import ResetPassword from "@/components/ResetPassword"
 
 function App() {
   return (
-    <LoginForm />
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+      <Route path="/reset" element={<ResetPassword />} />
+    </Routes>
   )
 }
 
